f0a3bb0d768fae792b65fe3839207f82b2891af3
so yeon <alicesysy@swu.ac.kr>
2021-10-09 17:24:32 +0900

    ➕[ADD] 세미나 프로젝트 추가

---
+++ Seminar/2nd-Seminar/2nd-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd-Seminar
+//
+//  Created by soyeon on 2021/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Seminar/2nd-Seminar/2nd-Seminar/YellowVC.swift
@@ -0,0 +1,29 @@
+//
+//  YellowVC.swift
+//  2nd-Seminar
+//
+//  Created by soyeon on 2021/10/09.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
