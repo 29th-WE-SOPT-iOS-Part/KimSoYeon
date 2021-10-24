6c1aa6078539fabd2ecdc74cd8cb58c6ea024a68
so yeon <alicesysy@swu.ac.kr>
2021-10-25 01:21:20 +0900

    🍱[ADD] 에셋 추가

---
+++ Assignment/Youtube-iOS/Youtube-iOS/Sources/VCs/Youtube/Cells/CVC/ChannelCVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelCVC.swift
+//  Youtube-iOS
+//
+//  Created by soyeon on 2021/10/25.
+//
+
+import UIKit
+
+class ChannelCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
