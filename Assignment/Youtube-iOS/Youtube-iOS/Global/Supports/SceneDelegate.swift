d561a6f2dee5333a5f6ab073f84d220afe111796
so yeon <alicesysy@swu.ac.kr>
2021-11-05 00:33:53 +0900

    [feat] 화면전환 효과 수정

--- Assignment/Youtube-iOS/Youtube-iOS/Global/Supports/SceneDelegate.swift
+++ Assignment/Youtube-iOS/Youtube-iOS/Global/Supports/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = UINavigationController(rootViewController: SignInVC())
         window?.makeKeyAndVisible()
     }
 
