74b8646101e08cd088ee3312eb6e756fae9c564b
so yeon <alicesysy@swu.ac.kr>
2021-12-18 13:11:22 +0900

    ➕[ADD] Podfile 추가

---
+++ Seminar/8th-Seminar/8th-Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8th-Seminar
+//
+//  Created by soyeon on 2021/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
